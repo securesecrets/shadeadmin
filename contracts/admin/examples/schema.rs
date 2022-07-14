c7fb94d684046fd50aefa939387a8fabd49fc6ba
hydropump3 <3ki2fiay@anonaddy.me>
2022-07-14 00:05:37 -0400

    adds schema gen

---
+++ contracts/admin/examples/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use shade_admin::admin::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+    }
+}
