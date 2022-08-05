9666c881974dd6c43a97601457c5bb41b96a4786
hydropump3 <3ki2fiay@anonaddy.me>
2022-08-05 11:38:29 -0400

    updates admi nauth

--- packages/multi_test/src/lib.rs
+++ packages/multi_test/src/lib.rs
@@ -6,4 +6,14 @@ mod test;
 pub mod multi {
     multi_derive::implement_multi!(AdminAuth, admin);
     pub use shade_admin::admin::*;
+
+    use shade_protocol::multi_test::App;
+    use shade_admin::InstantiateCallback;
+
+    /// Initializes an admin auth contract in multitest with superadmin as the superadmin.
+    pub fn init_admin_auth(app: &mut App, superadmin: &Addr) -> ContractInfo {
+        InstantiateMsg {
+            super_admin: Some(superadmin.clone().to_string()),
+        }.test_init(AdminAuth::default(), app, superadmin.clone(), "admin_auth", &[]).unwrap()
+    }
 }
